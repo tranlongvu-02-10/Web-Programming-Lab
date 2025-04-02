e58d9205927988de117a41208192e175e7f6f77e
Tran Long Vu <tranlongvu02102004@gmail.com>
2025-04-03 03:53:17 +0700

    Update

--- obj/Debug/net8.0/Lab04.WebsiteBanhang.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab04.WebsiteBanhang.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab04.WebsiteBanhang")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f68b28f36fbdc3462bdb88d7173695c52d1f19ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea41d5bf1b446a7f95ccb8d289ff882f5559df18")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab04.WebsiteBanhang")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab04.WebsiteBanhang")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
